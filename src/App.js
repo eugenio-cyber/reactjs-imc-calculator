160b2d14626bf594f44329afc4d36b92ba171163
eugenio-cyber <wesleyeugenio3@gmail.com>
2022-02-01 16:58:31 -0300

    Excluindo arquivos, adicao do componente e estilizacao

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [altura, setAltura] = useState();
+  const [peso, setPeso] = useState();
+  const [mesagem, setMensagem] = useState();
+
+  const calulo = () => {
+    const imc = peso / (altura * altura);
+    if (imc < 18.5) {
+      setMensagem(`Magreza, seu IMC é: ${imc.toFixed(2)}`);
+    } else if (imc >= 18.5 && imc < 24.9) {
+      setMensagem(`Normal, seu IMC é: ${imc.toFixed(2)}`);
+    } else if (imc >= 24.9 && imc < 30) {
+      setMensagem(`Sobrepeso, seu IMC é: ${imc.toFixed(2)}`);
+    } else if (imc > 30) {
+      setMensagem(`Obesidade, seu IMC é: ${imc.toFixed(2)}`);
+    }
+
+    setPeso("");
+    setAltura("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Claculadora IMC</h1>
+      <h4>Vamos calcular seu IMC!</h4>
+
+      <form className="input-area">
+        <label for="altura">Altura:</label>
+        <input
+          type="number"
+          placeholder="Digite sua altura, ex: 1.70"
+          id="altura"
+          value={altura}
+          onChange={(e) => {
+            setAltura(e.target.value);
+          }}
+        />
+      </form>
+      <form className="input-area">
+        <label for="peso">Peso:</label>
+        <input
+          type="number"
+          placeholder="Digite seu peso, ex: 80"
+          id="peso"
+          value={peso}
+          onChange={(e) => {
+            setPeso(e.target.value);
+          }}
+        />
+      </form>
+
+      <button onClick={calulo}>Calcular</button>
+
+      <span>
+        <b>Seu resultado:</b> {mesagem}.
+      </span>
     </div>
   );
 }
